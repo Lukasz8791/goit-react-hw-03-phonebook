df9d69d3c37ca55bd6fbd372725a26ae3d9d4559
Lukasz8791 <doktor8on@gmail.com>
2024-01-27 18:53:51 +0100

    phonebook3-09

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
