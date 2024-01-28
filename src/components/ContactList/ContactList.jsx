272c8a16a08ff66da983e51256943effc1578931
Lukasz8791 <doktor8on@gmail.com>
2024-01-28 17:28:38 +0100

    phonebook3-11

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ContactList.module.css';
 
-const ContactList = ({ contacts, filter }) => {
+const ContactList = ({ contacts, filter, onDelete }) => {
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
@@ -10,11 +11,24 @@ const ContactList = ({ contacts, filter }) => {
     <ul className={styles.ul}>
       {filteredContacts.map(contact => (
         <li key={contact.id} className={styles.li}>
-          {contact.name}
+          {contact.name} - {contact.number}
+          <button
+            type="button"
+            className={styles['delete-button']}
+            onClick={() => onDelete(contact.id)}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.array,
+  filter: PropTypes.string,
+  onDelete: PropTypes.func,
 };
 
 export default ContactList;
